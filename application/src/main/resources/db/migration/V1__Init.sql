1d156454042189cce42f34348dc502f12f8f7377
huisheng.jin <huisheng.jinge@gmail.com>
2021-07-14 00:58:40 +0800

    chore: 初始化数据

--- application/src/main/resources/db/migration/V1__Init.sql
+++ application/src/main/resources/db/migration/V1__Init.sql
@@ -8,3 +8,7 @@ CREATE TABLE `item`
     `type`  varchar(20)    NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `item`(`barcode`, `name`, `unit`, `price`, `type`)
+VALUES ('12345678', 'milk', 'L', '12.3', 1);
+
