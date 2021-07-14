c8bfcb0765df70bcc03ef1a76d4abb5c46f34aed
huisheng.jin <huisheng.jinge@gmail.com>
2021-07-14 13:01:39 +0800

    fix: 测试报错

--- application/src/test/java/cn/codingstyle/server/application/controller/ItemControllerTest.java
+++ application/src/test/java/cn/codingstyle/server/application/controller/ItemControllerTest.java
@@ -37,6 +37,7 @@ public class ItemControllerTest {
 
     @BeforeEach
     public void setUp() {
+        itemFactory.clear();
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
