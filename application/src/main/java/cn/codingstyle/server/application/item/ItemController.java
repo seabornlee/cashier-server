ca68d13098851f8231ea2946fdf3a10c32d75ec6
huisheng.jin <huisheng.jinge@gmail.com>
2021-07-18 22:18:20 +0800

    chore: remove api swagger annotation

--- application/src/main/java/cn/codingstyle/server/application/item/ItemController.java
+++ application/src/main/java/cn/codingstyle/server/application/item/ItemController.java
@@ -20,30 +20,25 @@ public class ItemController {
     }
 
     @PostMapping
-    @ApiOperation("创建商品")
     public void createItem(@RequestBody ItemForm itemForm) {
         itemService.newItem(itemForm.toRequest());
     }
 
-    @ApiOperation("获取商品")
     @GetMapping("/{barcode}")
     public Item getItem(@PathVariable String barcode) {
         return itemService.getItem(barcode);
     }
 
-    @ApiOperation("获取商品列表")
     @GetMapping
     public List<Item> getItems() {
         return itemService.getItems();
     }
 
-    @ApiOperation("更新商品")
     @PutMapping("/{id}")
     public void updateItem(@PathVariable Integer id, @RequestBody ItemForm itemForm) {
         itemService.updateItem(id, itemForm.toRequest());
     }
 
-    @ApiOperation("删除商品")
     @DeleteMapping("/{id}")
     public void deleteItem(@PathVariable Integer id) {
         itemService.deleteItem(id);
