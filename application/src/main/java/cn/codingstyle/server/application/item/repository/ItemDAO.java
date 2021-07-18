2e61ddfa179c840fd2f8dcf42d2c8f07edd4221e
huisheng.jin <huisheng.jinge@gmail.com>
2021-07-19 01:16:48 +0800

    chore: delete unused code

--- application/src/main/java/cn/codingstyle/server/application/item/repository/ItemDAO.java
+++
@@ -1,8 +0,0 @@
-package cn.codingstyle.server.application.item.repository;
-
-import cn.codingstyle.server.item.Item;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ItemDAO extends CrudRepository<ItemDO, Integer> {
-    Item findByBarcode(String barcode);
-}
